87d5219b12d58d2d453eda4b201bcc24b3414d0f
aakashrjrajpoot12 <aakashrjrajpoot@gmail.com>
2023-02-15 10:46:22 +0530

    2 files modified, 1 added

--- src/App.js
+++ src/App.js
@@ -1,11 +1,17 @@
 import React from "react";
 import "./style.css";
 
-export default function App() {
+export default class App extends React.Component {
+  mystyle={
+    color:'green'
+  };
+render(){
   return (
     <div>
-      <h1>Hello StackBlitz!</h1>
-      <p>Start editing to see some magic happen :)</p>
+      <h1 style={this.mystyle}>Hello StackBlitz!</h1>{/*Inline Styling*/} 
+       <p className='header'>Start editing to see some magic happen :)</p>
+      {/*External Styling*/} 
     </div>
   );
+}
 }
